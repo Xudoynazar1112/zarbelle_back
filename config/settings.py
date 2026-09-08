602c3311381d316ce6a6fe55617bb6227d69426b
Xudoynazar <saparovxudoynazar@gmail.com>
2026-09-09 00:13:43 +0500

    fix: remove recursive templates

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ CORS_ALLOW_ALL_ORIGINS = True  # yoki faqat frontend domenini qo‘shing
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
